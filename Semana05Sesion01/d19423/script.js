d0c6f485391b8bf1e0813627d4c8f666bc212241
Roberto Pineda <rpineda@x-codec.net>
2026-02-17 22:41:53 -0500

    Clase 17022026 segunda parte

--- Semana05Sesion01/d19423/script.js
+++ Semana05Sesion01/d19423/script.js
@@ -33,52 +33,167 @@ objGato01.despertar()
 
 //encapsulamiento
 
+//herencia
+
+//Polimorfismo
+
 let admin = true;
 
-class Television{
-    #precio = 0;
-    constructor(serie, marca,modelo,fechaCompra,formato,tamaño,sistemaOperativo){
-        this.serie = serie;
-        this.marca = marca;
-        this.modelo = modelo;
-        this.fechaCompra = fechaCompra;
-        this.formato = formato;
-        this.tamaño = tamaño;
-        this.sistemaOperativo = sistemaOperativo;
-    }
-    encender(){
-        console.log(`La television ${this.modelo} se esta encendiendo`)
-    }
-    apagar(){
-        console.log(`La television se esta apagando`)
-    }
-    setPrecio(newPrecio){
-        if(admin){
-            this.#precio = newPrecio
-        }
-    }
-    getPrecio(){
-        return `S/. ${this.#precio}`;
+
+
+// let objTV1 = new Television("00009","JVC","KTV0001","2025-10-25","LED",55,"GoogleTV");
+
+// console.log(objTV1.nombre);
+
+// let objTV2 = new Television("0009999","Samsung","Cristal","2022-09-01","OLED", 55,"Tizen");
+
+// console.log(objTV2.modelo);
+
+// objTV2.encender();
+// objTV1.encender();
+
+// objTV2.modelo = "Apple TV";
+
+// console.log(objTV2.modelo);
+
+// console.log(objTV1.getPrecio());
+
+// objTV1.setPrecio(900);
+
+// console.log(objTV1.getPrecio());
+
+class Dispositivo{
+    constructor(tipo, isActivo=false){
+        this.tipo = tipo;
+        this.isActivo = isActivo;
     }
 }
 
-let objTV1 = new Television("00009","JVC","KTV0001","2025-10-25","LED",55,"GoogleTV");
 
-console.log(objTV1.nombre);
+class Electrodomesticos{
+    #costo=0
+    constructor(serie, marca,modelo,conectividad,precio,fechaCompra= new Date()){
+        this.serie = serie;
+        this.marca = marca; 
+        this.modelo = modelo;
+        this.conectividad = conectividad;
+        this.precio = precio;
+        this.fechaCompra = fechaCompra;
+    }
+    encender(){
+        console.log(`El electrodomestico ${this.marca} se esta encendiendo`)
+    }
+    apagar(){
+        console.log(`El electrodomestico ${this.marca} se esta apagando`)
+    }
+    getCosto(){
+        if(admin)return this.#costo;
+    }
+    setCosto(newCosto){
+        if(admin) this.#costo = newCosto;
+    }
+    conectarAlInternet(dispositivo){
+        this.conectividad.forEach(element => {
+            if(element.tipo === dispositivo){
+                element.isActivo = true;
+                console.log(`El dispositivo de conectividad ${element.tipo} se ha activado`)
+            }
+            else{
+                element.isActivo = false;
+                console.log(`El dispositivo de conectividad ${element.tipo} se ha desactivado`)
+            }
+        });
+    }
+}
 
-let objTV2 = new Television("0009999","Samsung","Cristal","2022-09-01","OLED", 55,"Tizen");
+class LineaBlanca extends Electrodomesticos{
+    constructor(serie, marca,modelo,conectividad,precio,fechaCompra){
+        super(serie, marca,modelo,conectividad,precio,fechaCompra);
+    }
+}
 
-console.log(objTV2.modelo);
+class LineaMarron extends Electrodomesticos{
+    constructor(serie, marca,modelo,conectividad,precio,fechaCompra, entradas , salidas){
+        super(serie, marca,modelo,conectividad,precio,fechaCompra);
+        this.entradas = entradas;
+        this.salidas = salidas;
+    }
+    cambiarEntrada(entrada){
+        this.entradas.forEach(element=>{
+            if(element.tipo === entrada){
+                element.isActivo = true;
+                console.log(`El dispositivo de entrada ${element.tipo} se ha activado`)
+            }
+            else{
+                element.isActivo = false;
+                console.log(`El dispositivo de entrada ${element.tipo} se ha desactivado`)
+            }
+        })
+    }
+    cambiarSalida(salida){
+        this.salidas.forEach(element=>{
+            if(element.tipo === salida){
+                element.isActivo = true;
+                console.log(`El dispositivo de salida ${element.tipo} se ha activado`)
+            }
+            else{
+                element.isActivo = false;
+                console.log(`El dispositivo de salida ${element.tipo} se ha desactivado`)
+            }
+        })
+    }
+}
 
-objTV2.encender();
-objTV1.encender();
+class Television extends LineaMarron{
+    constructor(serie, marca,modelo,conectividad,precio,fechaCompra, entradas , salidas,formato,tamaño,sistemaOperativo){
+        super(serie, marca,modelo,conectividad,precio,fechaCompra, entradas , salidas);
+        this.formato = formato;
+        this.tamaño = tamaño;
+        this.sistemaOperativo =sistemaOperativo;
+    }
+    cambiarCanal(numeroCanal){
+        let exito = false;
+        this.entradas.forEach(element=>{
+            if(element.isActivo && element.tipo === "Cable"){
+                exito =true;
+                console.log(`La television ${this.marca} ${this.modelo} esta mostrando el canal ${numeroCanal}`);
+            }
+            if(!exito){
+                console.log(`La television ${this.marca} ${this.modelo} no puede realizar esta accion`);
+            }
+        })
+    }
+    encender(){
+        console.log(`La Television ${this.marca} ${this.modelo} se esta encendiendo`)
+    }
+}
 
-objTV2.modelo = "Apple TV";
 
-console.log(objTV2.modelo);
 
-console.log(objTV1.getPrecio());
+let objTV0001 = new Television("000001","JVC","KTV0001",[
+    new Dispositivo("WIFI"),
+    new Dispositivo("BT"),
+    new Dispositivo("LAN",true)
+],900.99,null,[
+    new Dispositivo("HDMI"),
+    new Dispositivo("Cable", true),
+    new Dispositivo("BT-IN")
+],[
+    new Dispositivo("Parlantes"),
+    new Dispositivo("BT-OUT")
+],"4K",55,"GoogleTV");
+console.log(objTV0001)
 
-objTV1.setPrecio(900);
 
-console.log(objTV1.getPrecio());
+objTV0001.apagar();
+objTV0001.conectarAlInternet("WIFI");
+objTV0001.cambiarEntrada("HDMI");
+
+objTV0001.cambiarEntrada("Cable");
+objTV0001.cambiarCanal(88);
+
+objTV0001.encender();
+
+let objLineaBlanca = new LineaBlanca("ttttt","Sony", "3333",[],99,null);
+
+objLineaBlanca.encender()
